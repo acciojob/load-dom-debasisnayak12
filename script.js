d76f27a51e3f7e561157740527b6d3c8731ec703
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-22 09:37:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 	//DOM
 	// Remove any existing child elements of the body tag
 	document.body.innerHTML = '';
-	//create element
 	const element = document.createElement('p');
 	element.textContent = 'DOM load success';
 	document.body.appendChild(element);
