dfc8ee589ba52ac8efb8a0ba77bad99e29f3a9b4
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-22 09:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+window.addEventListener('DOMContentLoaded', () => {
+	const element = document.createElement('p');
+	element.textContent = 'DOM load success';
+	document.body.appendChild(element);
+});
