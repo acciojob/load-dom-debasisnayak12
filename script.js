6b28aecc217239e2c50441d4c64e8d10eddf4a22
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-22 10:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 //your JS code here. If required.
-window.addEventListener('DOMContentLoaded', () => {
-	const bodyChildren = document.body.children;
-	if(bodyChildren.lenght > 0){
-		while(bodyChildren.length >0){
-			document.body.removeChild(bodyChildren[0]);
-		}
-	}
-	//DOM
-	// Remove any existing child elements of the body tag
-	document.body.innerHTML = '';
-	const element = document.createElement('p');
-	element.textContent = 'DOM load success';
-	document.body.appendChild(element);
-});
+// Check if the document is already loaded
+if (document.readyState === 'loading') {
+  // Wait for the DOMContentLoaded event to fire
+  document.addEventListener('DOMContentLoaded', () => {
+    // Check if the body is empty
+    if (document.body.childNodes.length === 0) {
+      const element = document.createElement('p');
+      element.textContent = 'DOM load success';
+      document.body.appendChild(element);
+    }
+  });
+} else {
+  // If the document is already loaded, check if the body is empty
+  if (document.body.childNodes.length === 0) {
+    const element = document.createElement('p');
+    element.textContent = 'DOM load success';
+    document.body.appendChild(element);
+  }
+}
