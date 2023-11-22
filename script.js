268c661b2443a6e019115e33bde62d8218ec12e2
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-22 16:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 //your JS code here. If required.
-// Check if the document is already loaded
-if (document.readyState === 'loading') {
-  // Wait for the DOMContentLoaded event to fire
-  document.addEventListener('DOMContentLoaded', () => {
-    // Check if the body is empty
-    if (document.body.childNodes.length === 0) {
-      const element = document.createElement('p');
-      element.textContent = 'DOM load success';
-      document.body.appendChild(element);
-    }
-  });
-} else {
-  // If the document is already loaded, check if the body is empty
-  if (document.body.childNodes.length === 0) {
-    const element = document.createElement('p');
-    element.textContent = 'DOM load success';
-    document.body.appendChild(element);
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+	const body = document.querySelector("body");
+	if(body.hasChildNodes()){
+		body.innerHTML = '';
+	}
+
+	const element = document.createElements("p");
+	element.textContent = "DOM load success";
+
+	body.appendChild(element);
+});
